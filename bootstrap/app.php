97744a695da1b2454753dac5c83c9ca4029050a8
GeorgeWarren1 <george.warren.pne@gmail.com>
2025-01-07 12:11:07 -0500

    يلعن اللله

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\AdminMiddleware::class,
-            'exclude-webhook-csrf' => \App\Http\Middleware\ExcludeWebhookFromCsrf::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
